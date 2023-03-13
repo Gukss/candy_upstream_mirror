e320ef0d4c5e89f15dde45e62a9d9716153dc5b7
Deokho Kim <demiacan@naver.com>
2023-03-13 17:40:23 +0900

    FEAT: FE_카카오 로그인 추가

--- FE/candy/lib/main.dart
+++ FE/candy/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:candy/screens/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: '29d8fb3c41f1f576f718af8398c6bf7e');
   runApp(const MyApp());
 }
 
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const Splash(),
     );
   }
 }
