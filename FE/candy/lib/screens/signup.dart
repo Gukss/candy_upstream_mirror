fc05d738d3c01f248e098a79564b0cd559b9b1c5
Deokho Kim <demiacan@naver.com>
2023-03-16 15:35:46 +0900

    FEAT: FE_signup 화면 및 작동 방법 변경

--- FE/candy/lib/screens/signup.dart
+++ FE/candy/lib/screens/signup.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:candy/widgets/signup/birth_date.dart';
+import 'package:candy/widgets/signup/gender.dart';
+import 'package:candy/widgets/signup/nickname.dart';
+import 'package:candy/widgets/ui/margin.dart';
+
 class Signup extends StatefulWidget {
   const Signup({super.key});
 
@@ -8,68 +13,63 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController nicknameController = TextEditingController();
-  String? nickname;
-  String? gender;
-  DateTime birthDate = DateTime.now();
+  late final List<int> yearList;
+  late final List<int> monthList;
+  late final List<int> dayList;
+  late Map<String, int> birthDate;
+  String gender = '';
 
-  void onGenderTap(String? value) {
+  // 등록 버튼 누르기
+  void onRegisterButtonPressed() {
+    print(nicknameController.text);
+    print(gender);
+    print(birthDate);
+  }
+
+  // 성별 선택
+  void onGenderButtonPressed(prevGender, clickedGender) {
+    if (prevGender == clickedGender) {
+      setState(() {
+        gender = '';
+      });
+    } else {
+      setState(() {
+        gender = clickedGender;
+      });
+    }
+  }
+
+  // 생년원일 선택
+  void onBirthDateSelected(String type, int value) {
     setState(() {
-      gender = value;
+      birthDate[type] = value;
     });
   }
 
-  // 등록 버튼 누르면 동작하는 함수
-  void onSubmitPressed() {
-    if (_formKey.currentState?.validate() ?? false) {
-      _formKey.currentState?.save();
-      print(nickname);
-      print(gender);
-      print(birthDate);
-    }
-  }
-
-  // 달력 누르면 동작하는 함수
-  void onCalendarPressed() async {
-    // 캘린더 열기
-    final DateTime? selectedDate = await showDatePicker(
-      context: context,
-      initialDate: birthDate,
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
-      locale: const Locale('ko', 'KR'),
-    );
-    if (selectedDate != null) {
-      setState(() {
-        birthDate = selectedDate;
-      });
-    }
+  @override
+  void initState() {
+    super.initState();
+    final today = DateTime.now();
+    yearList = [for (int i = 1950; i <= today.year; i++) i];
+    monthList = [for (int i = 1; i <= 12; i++) i];
+    dayList = [for (int i = 1; i <= 31; i++) i];
+    birthDate = {
+      'year': today.year,
+      'month': today.month,
+      'day': today.day,
+    };
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_rounded,
-            size: 24,
-          ),
-        ),
-        title: const Text(
-          '회원가입',
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
+        title: const Text('회원가입'),
         centerTitle: true,
         actions: [
           TextButton(
-            onPressed: onSubmitPressed,
+            onPressed: onRegisterButtonPressed,
             child: const Text(
               '등록',
               style: TextStyle(
@@ -80,131 +80,45 @@ class _SignupState extends State<Signup> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 8,
-          horizontal: 16,
-        ),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                '필수 정보',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-                textAlign: TextAlign.start,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 80, 16, 24),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 1,
               ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0.5),
-                  borderRadius: BorderRadius.circular(6),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            padding: const EdgeInsets.all(24),
+            width: double.infinity,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  '추가 정보 입력',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
                 ),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: nicknameController,
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsetsDirectional.symmetric(
-                          horizontal: 8,
-                          vertical: 8,
-                        ),
-                        border: InputBorder.none,
-                        hintText: '닉네임',
-                        hintStyle: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey.shade600,
-                        ),
-                      ),
-                    ),
-                    const Divider(
-                      thickness: 1,
-                      height: 0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                        vertical: 8,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            '성별',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey.shade600,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              TextButton(
-                                onPressed: () {},
-                                child: const Text('남'),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: const Text('여'),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Divider(
-                      thickness: 1,
-                      height: 0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            '생년월일',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey.shade600,
-                            ),
-                          ),
-                          SizedBox(
-                            child: Row(
-                              children: [
-                                Text(
-                                  '${birthDate.year} - ${birthDate.month} - ${birthDate.day}',
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 8,
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    onCalendarPressed();
-                                  },
-                                  icon: const Icon(
-                                    Icons.calendar_month,
-                                    size: 16,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                const Margin(marginType: 'height', size: 24),
+                Nickname(nicknameController: nicknameController),
+                const Margin(marginType: 'height', size: 16),
+                Gender(
+                  gender: gender,
+                  changeGender: onGenderButtonPressed,
                 ),
-              )
-            ],
+                const Margin(marginType: 'height', size: 16),
+                BirthDate(
+                  yearList: yearList,
+                  monthList: monthList,
+                  dayList: dayList,
+                  birthDate: birthDate,
+                  changeDate: onBirthDateSelected,
+                ),
+              ],
+            ),
           ),
         ),
       ),
