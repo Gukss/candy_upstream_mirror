dce7252994e300aa9575003fd81b13d46f074433
유영훈 <yyh7750@gmail.com>
2023-03-22 15:46:31 +0900

    FEAT : 찜하기 API 완료

--- BE/candy/src/main/java/com/project/candy/like/controller/LikeController.java
+++ BE/candy/src/main/java/com/project/candy/like/controller/LikeController.java
@@ -1,9 +1,11 @@
 package com.project.candy.like.controller;
 
+import com.project.candy.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * packageName    : com.project.candy.like.controller
@@ -16,4 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/like")
 @RequiredArgsConstructor
 public class LikeController {
+
+  private final LikeService likeService;
+
+  @PostMapping("/{beer-id}")
+  public ResponseEntity<?> createLikeBeer(
+          @PathVariable(name = "beer-id") Long beerId, @RequestBody String userEmail) {
+    likeService.createLikeBeer(beerId, userEmail);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{beer-id}")
+  public ResponseEntity<?> deleteLikeBeer(
+          @PathVariable(name = "beer-id") Long beerId, @RequestBody String userEmail) {
+    likeService.deleteLikeBeer(beerId, userEmail);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
 }
