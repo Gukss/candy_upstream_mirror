71442822b57dbe021c7fc1e6bb3f29591fd62564
송은지 <cjsdks518@naver.com>
2023-03-24 12:42:29 +0900

    FEAT: FE_dropbox별 맥주 내역 출력

--- FE/candy/lib/widgets/my_page/beer_encyclopedia/beer_list.dart
+++ FE/candy/lib/widgets/my_page/beer_encyclopedia/beer_list.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:candy/widgets/beer/beer_image_with_name.dart';
+import 'package:candy/widgets/ui/margin.dart';
 
 class BeerList extends StatelessWidget {
   final double imgBackgroundSize;
-  final int newbeerlist;
+  final List newbeerlist;
 
   const BeerList({
     super.key,
@@ -14,22 +15,53 @@ class BeerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            BeerImageWithName(
-              backColor: Colors.black,
-              backSize: imgBackgroundSize,
-              beerImgSrc: 'hello',
-              beerName: const {'name': 'hello'},
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 8,
-        )
-      ]),
+      child: ListView.separated(
+          separatorBuilder: (context, index) {
+            return const Margin(marginType: MarginType.width, size: 16);
+          },
+          itemCount: (newbeerlist.length / 3).ceil(),
+          itemBuilder: (context, index) {
+            return Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  BeerImageWithName(
+                    backColor: Colors.black,
+                    backSize: imgBackgroundSize,
+                    beerImgSrc: newbeerlist[index * 3]['imgSrc'],
+                    beerName: newbeerlist[index * 3]['name'],
+                  ),
+                  if (index * 3 + 1 < newbeerlist.length)
+                    BeerImageWithName(
+                      backColor: Colors.black,
+                      backSize: imgBackgroundSize,
+                      beerImgSrc: newbeerlist[index * 3 + 1]['imgSrc'],
+                      beerName: newbeerlist[index * 3 + 1]['name'],
+                    ),
+                  if (index * 3 + 1 >= newbeerlist.length)
+                    const SizedBox(
+                      height: 100,
+                      width: 100,
+                    ),
+                  if (index * 3 + 2 < newbeerlist.length)
+                    BeerImageWithName(
+                      backColor: Colors.black,
+                      backSize: imgBackgroundSize,
+                      beerImgSrc: newbeerlist[index * 3 + 2]['imgSrc'],
+                      beerName: newbeerlist[index * 3 + 2]['name'],
+                    ),
+                  if (index * 3 + 2 >= newbeerlist.length)
+                    const SizedBox(
+                      height: 100,
+                      width: 100,
+                    ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              )
+            ]);
+          }),
     );
   }
 }
