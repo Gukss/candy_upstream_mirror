62957aefdf533af79a1c408004e15d1b8235839d
유영훈 <yyh7750@gmail.com>
2023-03-26 01:35:46 +0900

    FEAT : 맥주 상세정보의 값들에 대한 연산 스케줄링 구현 완료

--- BE/candy/src/main/java/com/project/candy/beer/repository/BeerRepository.java
+++ BE/candy/src/main/java/com/project/candy/beer/repository/BeerRepository.java
@@ -1,11 +1,13 @@
 package com.project.candy.beer.repository;
 
+import com.project.candy.beer.dto.ReadBeerAvgFromReviewResponse;
 import com.project.candy.beer.dto.ReadBeerListResponse;
 import com.project.candy.beer.entity.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -37,14 +39,16 @@ public interface BeerRepository extends JpaRepository<Beer, Long> {
   List<ReadBeerListResponse> readAllBeerList(@Param("user_id") long userId);
 
   /**
-   * 검색을 한글 맥주 이름으로 검색했을 때 사용하는 메소드 
+   * 검색을 한글 맥주 이름으로 검색했을 때 사용하는 메소드
+   *
    * @param beerName : 맥주 한글 이름
    * @return
    */
   List<Beer> findAllByBeerKrNameContaining(String beerName);
-  
+
   /**
-   * 검색을 영어 맥주 이름으로 검색했을 때 사용하는 메소드 
+   * 검색을 영어 맥주 이름으로 검색했을 때 사용하는 메소드
+   *
    * @param beerName : 맥주 영어 이름
    * @return
    */
@@ -52,8 +56,23 @@ public interface BeerRepository extends JpaRepository<Beer, Long> {
 
   /**
    * 바코드로 맥주 조회
+   *
    * @param barcode
    * @return
    */
   Optional<Beer> findByBarcode(String barcode);
+
+  /**
+   * 맥주 상세정보의 통계값들을 계산하기 위한 메소드.
+   * 스케줄링 시 실행되며 모든 리뷰의 평균 값을 하루마다 갱신한다.
+   *
+   * @return
+   */
+  @Query(nativeQuery = true, value = "select beer_id, appearance, mouthfeel, flavor, aroma, overall, " +
+          "avg(appearance) as appearanceAvg, avg(mouthfeel) as mouthfeelAvg, avg(flavor) as flavorAvg, " +
+          "avg(aroma) as aromaAvg, avg(overall) as overallAvg from review " +
+          "group by beer_id, appearance, mouthfeel, flavor, aroma, overall with rollup " +
+          "having beer_id is not null and appearance is null and mouthfeel is null and flavor is null " +
+          "and aroma is null and overall is null;")
+  List<ReadBeerAvgFromReviewResponse> readAllBeerAvgList();
 }
