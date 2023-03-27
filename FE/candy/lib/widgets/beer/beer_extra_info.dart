52dc5288e9db19dccc60673bad13019448d1a4c5
송은지 <cjsdks518@naver.com>
2023-03-27 15:07:44 +0900

    FEAT: FE_info부분 brand 수정 및 flavor등 용어 변경

--- FE/candy/lib/widgets/beer/beer_extra_info.dart
+++ FE/candy/lib/widgets/beer/beer_extra_info.dart
@@ -1,22 +1,25 @@
+import 'package:flutter/material.dart';
+
 import 'package:candy/widgets/review/slide_bar.dart';
 import 'package:candy/widgets/ui/margin.dart';
-import 'package:flutter/material.dart';
 
 class BeerExtraInfo extends StatelessWidget {
   const BeerExtraInfo({
     super.key,
-    required this.freshNum,
-    required this.bodyNum,
-    required this.tasteNum,
+    required this.apperanceNum,
+    required this.mouthfeelNum,
+    required this.flavorNum,
+    required this.aromaNum,
     required this.readOnly,
     this.changeBody,
     this.changeFresh,
     this.changeTaste,
   });
 
-  final double freshNum;
-  final double bodyNum;
-  final double tasteNum;
+  final double apperanceNum;
+  final double mouthfeelNum;
+  final double flavorNum;
+  final double aromaNum;
   final bool readOnly;
   final Function? changeFresh;
   final Function? changeBody;
@@ -27,22 +30,29 @@ class BeerExtraInfo extends StatelessWidget {
     return Column(
       children: [
         SlideBar(
-          sliderCategory: '청량감',
-          sliderValue: freshNum,
+          sliderCategory: '색깔',
+          sliderValue: apperanceNum,
           readOnly: readOnly,
           changeSlider: changeFresh,
         ),
         const Margin(marginType: MarginType.height, size: 16),
         SlideBar(
           sliderCategory: '바디감',
-          sliderValue: bodyNum,
+          sliderValue: mouthfeelNum,
           readOnly: readOnly,
           changeSlider: changeBody,
         ),
         const Margin(marginType: MarginType.height, size: 16),
         SlideBar(
           sliderCategory: '맛',
-          sliderValue: tasteNum,
+          sliderValue: flavorNum,
+          readOnly: readOnly,
+          changeSlider: changeTaste,
+        ),
+        const Margin(marginType: MarginType.height, size: 16),
+        SlideBar(
+          sliderCategory: '향',
+          sliderValue: aromaNum,
           readOnly: readOnly,
           changeSlider: changeTaste,
         ),
