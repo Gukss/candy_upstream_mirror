59ce0186a9420b23b292ce0dd83a208255db71f5
최현호 <potato3884@naver.com>
2023-03-27 16:18:55 +0900

    Upload New File

---
+++ BE/flaskServer/app.py
@@ -0,0 +1,20 @@
+from flask import Flask  # 서버 구현을 위한 Flask 객체 import
+from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
+
+app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
+api = Api(app)  # Flask 객체에 Api 객체 등록
+
+
+@api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+class HelloWorld(Resource):
+    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        return {"hello": "world!"}
+
+@api.route('/reccomand')
+class ReccomandBeer():
+    def get(self):
+        return {"hello": "world!"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=90)
