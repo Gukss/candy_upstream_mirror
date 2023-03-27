c4f40bfa8341098b03705ed086328044eb84a670
송은지 <cjsdks518@naver.com>
2023-03-27 15:09:42 +0900

    FEAT: FE_디테일 페이지 api연결

--- FE/candy/lib/screens/beer_detail.dart
+++ FE/candy/lib/screens/beer_detail.dart
@@ -1,10 +1,12 @@
+import 'package:candy/api/beer_api_service.dart';
+import 'package:flutter/material.dart';
+
+import 'package:candy/models/beer/beer_detail_model.dart';
 import 'package:candy/widgets/app_bar/beer_detail_app_bar.dart';
 import 'package:candy/widgets/beer/beer_extra_info.dart';
 import 'package:candy/widgets/beer/beer_info.dart';
 import 'package:candy/widgets/review/beer_review_list.dart';
 import 'package:candy/widgets/ui/margin.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class BeerDetail extends StatelessWidget {
   final int beerId;
@@ -14,65 +16,68 @@ class BeerDetail extends StatelessWidget {
     required this.beerId,
   });
 
-  Future<Map<String, dynamic>> getBeerInfo() async {
-    const String basicUrl = 'j8b105.p.ssafy.io/api';
-    final Map<String, String> headers = {
-      'Content-Type': 'application/json',
-    };
-    http.get(
-      Uri.parse('$basicUrl/$beerId'),
-    );
-    return {};
+  Future<BeerDetailModel> beerdetail() async {
+    return await BeerApiService.getBeerDetailInfo(
+        email: 'ac@naver.com', beerId: beerId);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const BeerDetailAppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                color: Colors.grey,
+      body: FutureBuilder(
+        future: beerdetail(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        children: [
+                          BeerInfo(
+                            beerName: {
+                              'korean': snapshot.data!.beerNameKR,
+                              'english': snapshot.data!.beerNameEN,
+                            },
+                            beerImgSrc: snapshot.data!.beerImageUrl,
+                            country: snapshot.data!.countryNameKR,
+                            alcoholLevel: snapshot.data!.abv,
+                            beerType: snapshot.data!.style,
+                            rate: snapshot.data!.overall,
+                          ),
+                          const Margin(marginType: MarginType.height, size: 16),
+                          BeerExtraInfo(
+                            mouthfeelNum: snapshot.data!.mouthfeel,
+                            flavorNum: snapshot.data!.flavor,
+                            aromaNum: snapshot.data!.aroma,
+                            apperanceNum: snapshot.data!.appearance,
+                            readOnly: true,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Margin(marginType: MarginType.height, size: 32),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
-                      BeerInfo(
-                        beerName: {'korean': '카스 프레시', 'english': 'Cass Fresh'},
-                        beerImgSrc:
-                            'https://justliquor.com.au/2735/cass-fresh-beer-can-355ml.jpg',
-                        country: '한국',
-                        brandName: '하이트진로',
-                        alcoholLevel: 4.2,
-                        beerType: '밀맥주',
-                        rate: 3.5,
-                      ),
-                      Margin(marginType: MarginType.height, size: 16),
-                      BeerExtraInfo(
-                        bodyNum: 1,
-                        tasteNum: 1,
-                        freshNum: 1,
-                        readOnly: true,
-                      ),
+                      BeerReviewList(),
                     ],
                   ),
-                ),
-              ),
-              const Margin(marginType: MarginType.height, size: 32),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  BeerReviewList(),
                 ],
               ),
-            ],
-          ),
-        ),
+            ));
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
