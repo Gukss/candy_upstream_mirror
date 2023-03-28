5d8341de2ca3d90fbc686b696a5c0ba44935ccbc
송은지 <cjsdks518@naver.com>
2023-03-28 14:53:31 +0900

    FEAT: FE_calendar 음주색상 변화 및 한글 설치

--- FE/candy/lib/widgets/my_page/calendar.dart
+++ FE/candy/lib/widgets/my_page/calendar.dart
@@ -21,7 +21,14 @@ class _Calendar2State extends State<Calendar2> {
   // 이벤트가 있는 배열
   Map<DateTime, List<Event>> events = {
     DateTime.utc(2023, 3, 1): [Event('맥주1')],
-    DateTime.utc(2023, 3, 14): [Event('맥주3')]
+    DateTime.utc(2023, 3, 14): [Event('맥주3')],
+    DateTime.utc(2023, 3, 18): [Event('맥주3')],
+    DateTime.utc(2023, 3, 23): [Event('맥주3')],
+    DateTime.utc(2023, 3, 13): [Event('맥주3')],
+    DateTime.utc(2023, 3, 9): [Event('맥주3')],
+    DateTime.utc(2023, 3, 21): [Event('맥주3')],
+    DateTime.utc(2023, 3, 5): [Event('맥주3')],
+    DateTime.utc(2023, 3, 8): [Event('맥주3')],
   };
 
   List<Event> _getEventForDay(DateTime day) {
@@ -33,9 +40,9 @@ class _Calendar2State extends State<Calendar2> {
     return SafeArea(
         child: TableCalendar(
       focusedDay: focusedDay,
+      locale: 'ko-KR',
       firstDay: DateTime(1901),
       lastDay: DateTime(2101),
-      // locale: 'ko-KR',
       daysOfWeekHeight: 30,
       headerStyle: const HeaderStyle(
           headerMargin: EdgeInsets.only(left: 40, right: 40, bottom: 10),
@@ -44,8 +51,15 @@ class _Calendar2State extends State<Calendar2> {
           titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
       calendarStyle: const CalendarStyle(
           weekendTextStyle: TextStyle(color: Colors.red),
-          holidayTextStyle: TextStyle(color: Colors.blue),
-          todayTextStyle: TextStyle(fontWeight: FontWeight.bold)),
+          todayTextStyle: TextStyle(fontWeight: FontWeight.bold),
+          todayDecoration: BoxDecoration(
+            color: Color(0xFFFFEB3B),
+            shape: BoxShape.circle,
+          ),
+          selectedDecoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 204, 0),
+            shape: BoxShape.circle,
+          )),
 
       // 이벤트 있을 경우
       calendarBuilders: CalendarBuilders(
@@ -54,9 +68,12 @@ class _Calendar2State extends State<Calendar2> {
             return Container(
               width: 35,
               decoration: BoxDecoration(
-                  color: Colors.blue.withOpacity(0.2), shape: BoxShape.circle),
+                  color:
+                      const Color.fromARGB(255, 255, 204, 0).withOpacity(0.5),
+                  shape: BoxShape.circle),
             );
           }
+          return null;
         },
       ),
       onDaySelected: (selectedDay, focusedDay) {
