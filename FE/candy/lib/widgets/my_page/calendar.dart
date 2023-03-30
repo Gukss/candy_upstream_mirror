a60d12647ef5feee9882881038d47e4c473c0bdc
송은지 <cjsdks518@naver.com>
2023-03-30 17:41:43 +0900

    FEAT: FE_달력 키보드 올라올때 오류 수정

--- FE/candy/lib/widgets/my_page/calendar.dart
+++ FE/candy/lib/widgets/my_page/calendar.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:candy/api/user_api_service.dart';
+import 'package:candy/models/user/user_daily_data_model.dart';
+import 'package:intl/intl.dart';
+
 import 'package:table_calendar/table_calendar.dart';
 
 class Calendar2 extends StatefulWidget {
@@ -10,88 +14,76 @@ class Calendar2 extends StatefulWidget {
 }
 
 class _Calendar2State extends State<Calendar2> {
-  DateTime selectedDay = DateTime(
-    DateTime.now().year,
-    DateTime.now().month,
-    DateTime.now().day,
-  );
-
   DateTime focusedDay = DateTime.now();
 
-  // 이벤트가 있는 배열
-  Map<DateTime, List<Event>> events = {
-    DateTime.utc(2023, 3, 1): [Event('맥주1')],
-    DateTime.utc(2023, 3, 14): [Event('맥주3')],
-    DateTime.utc(2023, 3, 18): [Event('맥주3')],
-    DateTime.utc(2023, 3, 23): [Event('맥주3')],
-    DateTime.utc(2023, 3, 13): [Event('맥주3')],
-    DateTime.utc(2023, 3, 9): [Event('맥주3')],
-    DateTime.utc(2023, 3, 21): [Event('맥주3')],
-    DateTime.utc(2023, 3, 5): [Event('맥주3')],
-    DateTime.utc(2023, 3, 8): [Event('맥주3')],
-  };
+  Future<List<UserDailyDataModel>> events() async {
+    return await UserApiService.getUserDailyData(
+        year: focusedDay.year, month: focusedDay.month, email: 'ac@naver.com');
+  }
 
-  List<Event> _getEventForDay(DateTime day) {
-    return events[day] ?? [];
+  drunkDay(DateTime day, List<UserDailyDataModel> allday) {
+    final date = DateFormat('yyyy-MM-d').format(day);
+    for (int i = 0; i < allday.length; i++) {
+      if (date == allday[i].createdAt) {
+        return ['true'];
+      }
+    }
+    return [];
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: TableCalendar(
-      focusedDay: focusedDay,
-      locale: 'ko-KR',
-      firstDay: DateTime(1901),
-      lastDay: DateTime(2101),
-      daysOfWeekHeight: 30,
-      headerStyle: const HeaderStyle(
-          headerMargin: EdgeInsets.only(left: 40, right: 40, bottom: 10),
-          formatButtonVisible: false,
-          titleCentered: true,
-          titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-      calendarStyle: const CalendarStyle(
-          weekendTextStyle: TextStyle(color: Colors.red),
-          todayTextStyle: TextStyle(fontWeight: FontWeight.bold),
-          todayDecoration: BoxDecoration(
-            color: Color(0xFFFFEB3B),
-            shape: BoxShape.circle,
-          ),
-          selectedDecoration: BoxDecoration(
-            color: Color.fromARGB(255, 255, 204, 0),
-            shape: BoxShape.circle,
-          )),
+    return FutureBuilder(
+      future: events(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return SafeArea(
+              child: SingleChildScrollView(
+            child: TableCalendar(
+              focusedDay: focusedDay,
+              locale: 'ko-KR',
+              firstDay: DateTime(1901),
+              lastDay: DateTime.now(),
+              daysOfWeekHeight: 30,
+              headerStyle: const HeaderStyle(
+                  headerMargin:
+                      EdgeInsets.only(left: 40, right: 40, bottom: 10),
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                  titleTextStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+              calendarStyle: const CalendarStyle(
+                weekendTextStyle: TextStyle(color: Colors.red),
+                todayTextStyle: TextStyle(fontWeight: FontWeight.bold),
+                todayDecoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 205, 6),
+                  shape: BoxShape.circle,
+                ),
+              ),
 
-      // 이벤트 있을 경우
-      calendarBuilders: CalendarBuilders(
-        markerBuilder: (context, day, event) {
-          if (event.isNotEmpty) {
-            return Container(
-              width: 35,
-              decoration: BoxDecoration(
-                  color:
-                      const Color.fromARGB(255, 255, 204, 0).withOpacity(0.5),
-                  shape: BoxShape.circle),
-            );
-          }
-          return null;
-        },
-      ),
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          this.selectedDay = selectedDay;
-          this.focusedDay = focusedDay;
-        });
+              // 이벤트 있을 경우
+              calendarBuilders: CalendarBuilders(
+                markerBuilder: (context, day, event) {
+                  if (event.isNotEmpty) {
+                    return Container(
+                      width: 35,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 196, 0)
+                              .withOpacity(0.4),
+                          shape: BoxShape.circle),
+                    );
+                  }
+                  return null;
+                },
+              ),
+              eventLoader: (day) {
+                return drunkDay(day, snapshot.data!);
+              },
+            ),
+          ));
+        }
+        return const SizedBox();
       },
-      selectedDayPredicate: (day) {
-        return isSameDay(selectedDay, day);
-      },
-      eventLoader: _getEventForDay,
-    ));
+    );
   }
-}
-
-class Event {
-  String title;
-
-  Event(this.title);
 }
