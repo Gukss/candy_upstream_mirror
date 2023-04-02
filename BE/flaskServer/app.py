f751e2523c40353350186c77201543838c75e03b
최현호 <potato3884@naver.com>
2023-04-02 16:39:30 +0900

    Fix : 플라스크에서의 시간 관련 라이브러리 설치 수정

--- BE/flaskServer/app.py
+++ BE/flaskServer/app.py
@@ -1,6 +1,8 @@
 from flask import Flask ,   request # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
+from pytz import timezone
 import model
 import logging 
 import pymysql
@@ -26,6 +28,12 @@ schedule.start()
 class HelloWorld(Resource):
     def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
         return {"hello": "world!"}
+
+@api.route('/checkTime')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
+class HelloWorld(Resource):
+    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
+        return {"curTime": str(datetime.datetime.now(timezone('Asia/Seoul')))}
+
 
 @api.route('/reccomand')
 class ReccomandBeer(Resource):
